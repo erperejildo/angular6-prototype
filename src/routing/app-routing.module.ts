f7d7afe8ab67c63732a0e6174f9d05d611bd2274
Daniel R <d15_1_89@msn.com>
2018-07-13 10:54:32 +0100

    finished routing

--- src/routing/app-routing.module.ts
+++ src/routing/app-routing.module.ts
@@ -2,17 +2,16 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
 import { UpgradeBusinessComponent } from '../components/upgrade-business/upgrade-business.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'upgrade', component: UpgradeBusinessComponent },
-  // {
-  //   path: '', redirectTo: '/upgrade',
-  //   pathMatch: 'full'
-  // }, // root page
+  { path: '**', component: PageNotFoundComponent },
+  { path: '404', component: PageNotFoundComponent }
   //   // { path: 'passing-params/:id', component: test }
-  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -21,4 +20,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 // avoid import statement duplication
-export const routingComponents = [UpgradeBusinessComponent, PageNotFoundComponent];
+export const routingComponents = [HomeComponent, UpgradeBusinessComponent, PageNotFoundComponent];
