8440ed7e84e4be11782335e6a9c423f7433659da
Daniel R <d15_1_89@msn.com>
2018-07-17 11:36:00 +0100

    Dynamic list and option selection

--- src/components/upgrade-business/upgrade-business.component.ts
+++ src/components/upgrade-business/upgrade-business.component.ts
@@ -11,6 +11,8 @@ export class UpgradeBusinessComponent {
   @ViewChild('step2') step2: ElementRef;
   animator1: AnimationBuilder;
   animator2: AnimationBuilder;
+  upgradeOptions: Array<object>;
+  selectedUpgradeOption: 0;
 
   constructor(animationService: AnimationService) {
     this.animator1 = animationService.builder();
@@ -20,6 +22,22 @@ export class UpgradeBusinessComponent {
     this.animator2 = animationService.builder();
     this.animator2.pin = false;
     this.animator2.reject = false;
+
+    // getting data "dinamically"
+    this.upgradeOptions = [
+      {
+        description: 'Outbound flight',
+        price: 1082
+      },
+      {
+        description: 'Inbound flight',
+        price: 1172
+      },
+      {
+        description: 'Both ways',
+        price: 1742
+      }
+    ];
   }
 
   animate() {
@@ -27,5 +45,10 @@ export class UpgradeBusinessComponent {
       // Animation finished
     });
     this.animator2.setType('slideInRight').show(this.step2.nativeElement);
+  }
+
+  selectUpgradeOption(index) {
+    this.selectedUpgradeOption = index;
+    console.log('option selected: ', this.upgradeOptions[index]);
   }
 }
