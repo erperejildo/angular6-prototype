2e6752baa9a1904f49d6ac4cbd979a948249b6fa
Daniel R <d15_1_89@msn.com>
2018-07-18 11:54:16 +0100

    Removed bootstrap navbar from Angular

--- angularJS/client/app/app.module.js
+++ angularJS/client/app/app.module.js
@@ -5,5 +5,5 @@ import appComponent from './app.component';
 import ComponentsModule from './components/components';
 
 angular.module('app', [
-  ComponentsModule.name
+    ComponentsModule.name
 ]).component('app', appComponent);
