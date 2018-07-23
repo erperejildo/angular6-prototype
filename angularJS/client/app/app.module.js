7c94a363f90fc0684afa9c7007c0fb368c867754
Safeer Hayat <saf_hayat94@hotmail.com>
2018-07-23 15:36:08 +0100

    WIP

--- angularJS/client/app/app.module.js
+++ angularJS/client/app/app.module.js
@@ -3,5 +3,9 @@ import "normalize.css";
 import angular from "angular";
 import appComponent from "./app.component";
 import ComponentsModule from "./components/components";
+import reactComponent from "./components/react-component/react-component";
+import { react2angular } from "react2angular";
 
-angular.module("app", [ComponentsModule.name]).component("app", appComponent);
+angular
+  .module("app", [ComponentsModule.name])
+  .component("reactComponent", react2angular(reactComponent, []));
