d3b8e55d2a5a4adb7b8eb8596ab45f93da1e695c
Daniel R <d15_1_89@msn.com>
2018-07-23 15:56:42 +0100

    Refactoring

--- angularJS/client/app/app.component.js
+++ angularJS/client/app/app.component.js
@@ -1,6 +1,5 @@
 import template from "./app.component.html";
 import "./app.component.scss";
-import reactComponent from "./components/react-component/react-component";
 
 const AppComponent = {
     template
